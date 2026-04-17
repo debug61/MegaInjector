480cc4be10dd89a829550eb3f3f0620d07d09b8a
Nikolai <nechaev-kolya-955@mail.ru>
2026-04-17 22:59:21 +0500

    Initial Commit

---
+++ src/main/java/com/debug61/mcsbotnet/RuntimeInjector.java
@@ -0,0 +1,49 @@
+package com.debug61.mcsbotnet;
+
+import java.io.File;
+import java.lang.instrument.Instrumentation;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class RuntimeInjector
+{
+
+    private static Instrumentation instrumentation;
+
+    private static final String mainClassName = "net.pandaew.reconnect.VariableProviderReconnect";
+
+    public static void premain(String agentArgs, Instrumentation inst) {
+        instrumentation = inst;
+    }
+
+    public static void agentmain(String agentArgs, Instrumentation inst) {
+        System.out.println("[MegaInjector] Агент подключен к работающему процессу");
+        instrumentation = inst;
+
+        try {
+            loadReconnectModule(agentArgs);
+        } catch (Exception e) {
+            System.out.println("[MegaInjector] Ошибка при загрузке модуля");
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private static void loadReconnectModule(String modulePath) throws Exception {
+        File moduleFile = new File(modulePath);
+        URL moduleURL = moduleFile.toURI().toURL();
+        URLClassLoader loader = new URLClassLoader(
+                new URL[]{moduleURL}, ClassLoader.getSystemClassLoader()
+        );
+
+        Class<?> reconnectClass = loader.loadClass(mainClassName);
+
+        Object moduleInstance = reconnectClass.getDeclaredConstructor().newInstance();
+
+        reconnectClass.getMethod("onInit").invoke(moduleInstance);
+
+        System.out.println("[MegaInjector] Модуль " + mainClassName + " успешно инициализирован");
+    }
+
+}
